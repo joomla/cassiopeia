7618aaef82528e1854a421cc624717a21a2e210c
Richard Fath <richard67@users.noreply.github.com>
2020-10-28 20:11:31 +0100

    Auto-collapse metismenu when click outside of menu

--- templates/cassiopeia/js/mod_menu/menu-metismenu.js
+++ templates/cassiopeia/js/mod_menu/menu-metismenu.js
@@ -3,8 +3,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	allMenus.forEach(menu => {
 		// eslint-disable-next-line no-new, no-undef
-		new MetisMenu(menu, {
+		const mm = new MetisMenu(menu, {
 			triggerElement: 'button.mm-toggler'
+		}).on("shown.metisMenu", function(event) {
+			window.addEventListener("click", function mmClick(e) {
+				if (!event.target.contains(e.target)) {
+					mm.hide(event.detail.shownElement);
+					window.removeEventListener("click", mmClick);
+				}
+			});
 		});
 	});
 });
