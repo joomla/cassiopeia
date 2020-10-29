e6c0c1d6df2bed6df0037e7c5274f76d587ef0d5
Richard Fath <richard67@users.noreply.github.com>
2020-10-29 14:20:09 +0100

    Add doc block to the javascript file

--- build/media_source/mod_menu/js/menu-metismenu.es6.js
+++ build/media_source/mod_menu/js/menu-metismenu.es6.js
@@ -1,3 +1,11 @@
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ * @copyright   (C) 2020 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @since       4.0
+ */
+
 document.addEventListener('DOMContentLoaded', () => {
   const allMenus = document.querySelectorAll('ul.mod-menu_metismenu');
 
