055464b6e33801fc440dfdc34bb7f0f484233e19
Richard Fath <richard67@users.noreply.github.com>
2020-11-07 11:12:58 +0100

    Fix js, add doc block, add minified js

--- templates/cassiopeia/js/mod_menu/menu-metismenu.js
+++ templates/cassiopeia/js/mod_menu/menu-metismenu.js
@@ -1,11 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const allMenus = document.querySelectorAll('ul.mod-menu_metismenu');
-
-  allMenus.forEach((menu) => {
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ * @copyright   (C) 2020 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @since       4.0
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  var allMenus = document.querySelectorAll('ul.mod-menu_metismenu');
+  allMenus.forEach(function (menu) {
     // eslint-disable-next-line no-new, no-undef
-    const mm = new MetisMenu(menu, {
-      triggerElement: 'button.mm-toggler',
-    }).on('shown.metisMenu', (event) => {
+    var mm = new MetisMenu(menu, {
+      triggerElement: 'button.mm-toggler'
+    }).on('shown.metisMenu', function (event) {
       window.addEventListener('click', function mmClick(e) {
         if (!event.target.contains(e.target)) {
           mm.hide(event.detail.shownElement);
